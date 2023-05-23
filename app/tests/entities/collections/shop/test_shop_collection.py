97db346a3900ad4e49749422710dccb5290ba220
SW <aliwo@naver.com>
2023-05-23 23:26:23 +0900

    intersects 쿼리 해보기 & 테스트. dict 대신 dto 를 써야 하는 이유

--- app/tests/entities/collections/shop/test_shop_collection.py
+++ app/tests/entities/collections/shop/test_shop_collection.py
@@ -1,5 +1,7 @@
+import asyncio
+
 from app.entities.category.category_codes import CategoryCode
-from app.entities.collections.geo_json import GeoJsonPolygon
+from app.entities.collections.geo_json import GeoJsonPoint, GeoJsonPolygon
 from app.entities.collections.shop.shop_collection import ShopCollection
 from app.entities.collections.shop.shop_document import ShopDeliveryAreaSubDocument
 
@@ -27,3 +29,68 @@ async def test_shop_insert_one() -> None:
     assert result["delivery_areas"] == [
         {"poly": {"type": "Polygon", "coordinates": [[[0, 0], [0, 10], [10, 10], [10, 0], [0, 0]]]}}
     ]
+
+
+async def test_shop_point_intersects() -> None:
+    # Given
+    await asyncio.gather(
+        ShopCollection.insert_one(
+            "치킨집",
+            [CategoryCode.CHICKEN],
+            [
+                ShopDeliveryAreaSubDocument(
+                    poly=GeoJsonPolygon(coordinates=[[[0, 0], [0, 10], [10, 10], [10, 0], [0, 0]]]),
+                )
+            ],
+        ),
+        ShopCollection.insert_one(
+            "피자집",
+            [CategoryCode.PIZZA],
+            [
+                ShopDeliveryAreaSubDocument(
+                    poly=GeoJsonPolygon(coordinates=[[[0, 0], [0, 2], [2, 2], [2, 0], [0, 0]]]),
+                )
+            ],
+        ),
+    )
+
+    # When
+    result = await ShopCollection.point_intersects(GeoJsonPoint(coordinates=[5, 5]))
+
+    # Then
+    assert len(result) == 1
+    assert result[0].name == "치킨집"
+
+
+async def test_shop_point_intersects_when_it_has_multiple_delivery_area() -> None:
+    # Given
+    await asyncio.gather(
+        ShopCollection.insert_one(
+            "치킨집",
+            [CategoryCode.CHICKEN],
+            [
+                ShopDeliveryAreaSubDocument(
+                    poly=GeoJsonPolygon(coordinates=[[[0, 0], [0, 10], [10, 10], [10, 0], [0, 0]]]),
+                ),
+                ShopDeliveryAreaSubDocument(
+                    poly=GeoJsonPolygon(coordinates=[[[0, 0], [0, 1], [1, 1], [1, 0], [0, 0]]]),
+                ),
+            ],
+        ),
+        ShopCollection.insert_one(
+            "피자집",
+            [CategoryCode.PIZZA],
+            [
+                ShopDeliveryAreaSubDocument(
+                    poly=GeoJsonPolygon(coordinates=[[[0, 0], [0, 2], [2, 2], [2, 0], [0, 0]]]),
+                ),
+            ],
+        ),
+    )
+
+    # When
+    result = await ShopCollection.point_intersects(GeoJsonPoint(coordinates=[5, 5]))
+
+    # Then
+    assert len(result) == 1
+    assert result[0].name == "치킨집"
