bc4e06edd7dc8d0cb72012b0cca5e7a601c7a164
SW <aliwo@naver.com>
2023-06-14 21:53:50 +0900

    intersects 구현, dict 대신 dto 를 써야 하는 이유

--- app/entities/collections/shop/shop_collection.py
+++ app/entities/collections/shop/shop_collection.py
@@ -1,8 +1,10 @@
 from dataclasses import asdict
+from typing import Any
 
 from motor.motor_asyncio import AsyncIOMotorCollection
 
 from app.entities.category.category_codes import CategoryCode
+from app.entities.collections.geo_json import GeoJsonPoint, GeoJsonPolygon
 from app.entities.collections.shop.shop_document import (
     ShopDeliveryAreaSubDocument,
     ShopDocument,
@@ -12,6 +14,15 @@ from app.utils.mongo import db
 
 class ShopCollection:
     _collection = AsyncIOMotorCollection(db, "shops")
+
+    @classmethod
+    async def point_intersects(cls, point: GeoJsonPoint) -> list[ShopDocument]:
+        return [
+            cls._parse(result)
+            for result in await cls._collection.find(
+                {"delivery_areas.poly": {"$geoIntersects": {"$geometry": asdict(point)}}}
+            ).to_list(length=None)
+        ]
 
     @classmethod
     async def insert_one(
@@ -29,4 +40,16 @@ class ShopCollection:
             name=name,
             category_codes=category_codes,
             delivery_areas=delivery_areas,
+        )
+
+    @classmethod
+    def _parse(cls, result: dict[Any, Any]) -> ShopDocument:
+        return ShopDocument(
+            _id=result["_id"],
+            name=result["name"],
+            delivery_areas=[
+                ShopDeliveryAreaSubDocument(poly=GeoJsonPolygon(coordinates=delivery_area["poly"]["coordinates"]))
+                for delivery_area in result["delivery_areas"]
+            ],
+            category_codes=[CategoryCode(category_code) for category_code in result["category_codes"]],
         )
