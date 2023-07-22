fc310178f5468aac5403bcaed30f9c0df6df50bc
SW <aliwo@naver.com>
2023-07-22 16:06:55 +0900

    위도 경도의 제한 print 제거

--- app/dtos/category/coordinates_request.py
+++ app/dtos/category/coordinates_request.py
@@ -9,7 +9,3 @@ class CoordinatesRequest:
     def __post_init__(self) -> None:
         self.longitude = round(self.longitude, 5)
         self.latitude = round(self.latitude, 5)
-
-
-coordinates_request = CoordinatesRequest(longitude=127.005926, latitude=37.490061)
-print(coordinates_request.longitude, coordinates_request.latitude)
