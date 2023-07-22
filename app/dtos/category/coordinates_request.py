66b22cb1d48cbac334c2c578817d06fb85230659
SW <aliwo@naver.com>
2023-07-22 17:50:42 +0900

    distinct router test

--- app/dtos/category/coordinates_request.py
+++ app/dtos/category/coordinates_request.py
@@ -1,10 +1,13 @@
 import dataclasses
+from typing import Annotated
+
+from fastapi import Query
 
 
 @dataclasses.dataclass
 class CoordinatesRequest:
-    longitude: float
-    latitude: float
+    longitude: Annotated[float, Query(..., example=127.00592, gt=124, lt=132)]
+    latitude: Annotated[float, Query(..., example=37.49006, gt=33, lt=39)]
 
     def __post_init__(self) -> None:
         self.longitude = round(self.longitude, 5)
