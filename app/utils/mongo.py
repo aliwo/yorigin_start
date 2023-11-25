c1d506b4c5e920b0f425833f6c4fbfb7bb0752aa
jyh <jyh6197@ex-em.com>
2023-11-25 15:00:41 +0900

    feat: Add Mongodb Connect Sample

--- app/utils/mongo.py
+++ app/utils/mongo.py
@@ -2,7 +2,24 @@ import os
 
 from motor.motor_asyncio import AsyncIOMotorClient
 
-DATABASE_NAME = os.environ.get("MONGO_DATABASE", "yorigin")
+def create_mongo_url(host: str='localhost', port: int=27017):
+    return f'mongodb://{host}:{port}'
 
-client = AsyncIOMotorClient()
+DATABASE_NAME = os.environ.get("MONGO_DATABASE", "yorigin")
+HOST = os.environ.get("MONGO_HOST", "localhost")
+PORT = os.environ.get("MONGO_PORT", 27017)
+
+client = AsyncIOMotorClient(create_mongo_url(HOST, PORT)) # db 를 외부 서버에 둠에 따라 이와 같이  환경변수를 통해 세팅되도록 수정하였다.
 db = client[DATABASE_NAME]
+
+
+
+import asyncio
+
+
+async def print_mongo_version():
+    status = await client.test.command("serverStatus")
+    print(status["version"])
+
+
+asyncio.run(print_mongo_version())
