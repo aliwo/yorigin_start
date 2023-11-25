97f8f19bcab773d7e5c7a291ed0d33abc487d244
jyh <jyh6197@ex-em.com>
2023-11-25 15:05:15 +0900

    feat: Change Format from black

--- app/utils/mongo.py
+++ app/utils/mongo.py
@@ -2,16 +2,17 @@ import os
 
 from motor.motor_asyncio import AsyncIOMotorClient
 
-def create_mongo_url(host: str='localhost', port: int=27017):
-    return f'mongodb://{host}:{port}'
+
+def create_mongo_url(host: str = "localhost", port: int = 27017):
+    return f"mongodb://{host}:{port}"
+
 
 DATABASE_NAME = os.environ.get("MONGO_DATABASE", "yorigin")
 HOST = os.environ.get("MONGO_HOST", "localhost")
 PORT = os.environ.get("MONGO_PORT", 27017)
 
-client = AsyncIOMotorClient(create_mongo_url(HOST, PORT)) # db 를 외부 서버에 둠에 따라 이와 같이  환경변수를 통해 세팅되도록 수정하였다.
+client = AsyncIOMotorClient(create_mongo_url(HOST, PORT))  # db 를 외부 서버에 둠에 따라 이와 같이  환경변수를 통해 세팅되도록 수정하였다.
 db = client[DATABASE_NAME]
-
 
 
 import asyncio
